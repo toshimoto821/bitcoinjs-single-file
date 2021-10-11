ae3b9ab8bf433c25c28d25d55212bb89b5364098
tj <tj@digitalarsenal.io>
2021-10-11 17:27:35 -0400

    added min

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
 
-export default {
+export default [{
     input: './bitcoinjs.js',
     output: {
         file: 'output/bitcoin.mjs',
         format: 'esm'
     },
     plugins: [commonjs()]
-};
+}, {
+    input: './bitcoinjs.js',
+    output: {
+        file: 'output/bitcoin.min.mjs',
+        format: 'esm'
+    },
+    plugins: [commonjs(), terser()]
+}];
