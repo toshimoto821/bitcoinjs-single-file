217186d5903b918cd257481b3079f2145a519585
tjkoury <tj@digitalarsenal.io>
2021-10-17 19:30:10 -0400

    added bip38 bip39

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var bitcoin_js = require('bitcoinjs-lib')
-bitcoin_js.Buffer = require('safe-buffer').Buffer
+var bitcoin_js = require("bitcoinjs-lib");
+bitcoin_js.Buffer = require("safe-buffer").Buffer;
+bitcoin_js.bip38 = require("bip38");
+bitcoin_js.bip39 = require("bip39");
+
 module.exports = bitcoin_js;
