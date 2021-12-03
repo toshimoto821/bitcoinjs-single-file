54ef6a4b8d07e284e615de48a981a0395b17cdc0
tj <tj@digitalarsenal.io>
2021-12-03 08:28:57 -0500

    added bitcoinjs-message

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var bitcoin_js = require("bitcoinjs-lib");
 bitcoin_js.Buffer = require("safe-buffer").Buffer;
 bitcoin_js.bip38 = require("bip38");
 bitcoin_js.bip39 = require("bip39");
-
+bitcoin_js.message = require('bitcoinjs-message');
 module.exports = bitcoin_js;
